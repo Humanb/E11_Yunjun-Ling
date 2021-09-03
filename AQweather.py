7ab71b50cd10f3a9ae5f22a96d17853de9637168
Ali Hanks <alihanks@gmail.com>
2021-09-02 20:57:55 -0700

    New data

--- AQweather.py
+++ AQweather.py
@@ -22,40 +22,36 @@ sleep_time = int(sys.argv[2])
 
 current_time = start_time
 
-sensorFile = open('sensor.csv', 'w')
+dataFile = open(sys.argv[4], 'w')
+dataWriter = csv.writer(dataFile)
 
-sensorWriter = csv.writer(sensorFile)
 
-header = ['time', 'temperature', 'pressure']
-sensorWriter.writerow(header)
+header = ['time', 'temperature', 'pressure', 'humidity', 'PM1', 'PM2', 'PM3']
+dataWriter.writerow(header)
 
 port = serial.Serial('/dev/serial0')
 
 
-AQFile = open('AQ', 'w')
-AQwriter = csv.writer(AQFile)
-
-run_time = int(sys.argv[1])
+delay_time = int(sys.argv[3])
+run_time = int(sys.argv[1])+int(sys.argv[3])
 stop_time = start_time+run_time
 
+time.sleep(delay_time)
 
 while current_time < stop_time:
-    data_list = [current_time, bme280.temperature, bme280.pressure]
-    sensorWriter.writerow(data_list)
+    
     current_time = time.time()
     Data= port.read(32)
     PM1 = int.from_bytes(Data[4:6], byteorder='big')
     PM2 = int.from_bytes(Data [6:8], byteorder='big')
     PM3 = int.from_bytes(Data [8:10], byteorder='big')
+    data_list = [current_time, bme280.temperature, bme280.pressure, bme280.relative_humidity, PM1, PM2, PM3]
+    dataWriter.writerow(data_list)
     print("\nTemperature: %0.1f C" % bme280.temperature)
     print("Humidity: %0.1f %%" % bme280.relative_humidity)
     print("Pressure: %0.1f hPa" % bme280.pressure)
     print("Altitude = %0.2f meters" % bme280.altitude)
-    data_list1 = [current_time, PM1, PM2, PM3]
-    AQwriter.writerow(data_list1)
-    current_time = time.time()
     print ("Data: ", PM1, PM2, PM3)
-    #print(int(sys.argv[1]))
     time.sleep(sleep_time)
     
 
@@ -64,3 +60,4 @@ while current_time < stop_time:
 
 	
     
+
